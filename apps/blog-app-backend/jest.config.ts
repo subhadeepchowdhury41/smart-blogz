3339263de6d07d3d4487c1434aa66e887a94dffb
subhadeep.chowdhury <subhadeep.chowdhury@webileapps.com>
2025-03-08 23:59:48 +0530

    Initial commit

---
+++ apps/blog-app-backend/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'blog-app-backend',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/blog-app-backend',
+};
